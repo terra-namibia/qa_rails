76da49d390d497b68c6731519099ffa999a623a7
terra-namibia <tera9319@yahoo.co.jp>
2022-01-28 22:51:55 +0900

    add Cors origins

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,14 @@ module QaRails
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+          origins "http://localhost:3000/", "https://qa-next.vercel.app/"
+          resource "*",
+          headers: :any,
+          methods: [:get, :post, :options, :head]
+      end
+    end
   end
 end
