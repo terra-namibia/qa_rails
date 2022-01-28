47b7ca5851c14342425fc6aa02d0b03d0f9cd83c
terra-namibia <tera9319@yahoo.co.jp>
2022-01-28 22:52:37 +0900

    add column score to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_26_090434) do
+ActiveRecord::Schema.define(version: 2022_01_28_133354) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2022_01_26_090434) do
     t.string "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "score"
   end
 
 end
