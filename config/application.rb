c90f572f75aa6e12067ff3ad2a5cb2d9a325342a
terra-namibia <tera9319@yahoo.co.jp>
2022-01-29 15:05:59 +0900

    fixed bug

--- config/application.rb
+++ config/application.rb
@@ -26,7 +26,7 @@ module QaRails
 
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-          origins "http://localhost:3000/", "https://qa-next.vercel.app/"
+          origins "localhost:3000", "https://qa-next.vercel.app/"
           resource "*",
           headers: :any,
           methods: [:get, :post, :options, :head]
